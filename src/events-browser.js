c3be4b7c8dc49699ac39b334f5886e27cbb46129
Lucas Nascimento <lucas.debarros@b2wdigital.com>
2016-05-12 19:31:18 -0300

    fix(polyfill): adjust dep

--- src/events-browser.js
+++ src/events-browser.js
@@ -1,6 +1,6 @@
 "use strict";
 
-require('./events-browser-polifyll');
+require('./events-browser-polyfill');
 
 const on = (evt, params) => window.addEventListener(evt, params);
 const emit = (evt, params) => window.dispatchEvent(new window.CustomEvent(evt, params));
