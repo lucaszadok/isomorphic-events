9e8aaa79d44a1238d69f8003255bca5f6a1b798e
Lucas Nascimento <lucas.debarros@b2wdigital.com>
2016-05-16 12:30:29 -0300

    fix(browser): remove arrow fn

--- src/events-browser.js
+++ src/events-browser.js
@@ -2,8 +2,13 @@
 
 require('./events-browser-polyfill');
 
-const on = (evt, params) => window.addEventListener(evt, params);
-const emit = (evt, params) => window.dispatchEvent(new window.CustomEvent(evt, params));
+const on = function(evt, params) {
+  return window.addEventListener(evt, params);
+};
+
+const emit = function(evt, params) {
+  window.dispatchEvent(new window.CustomEvent(evt, params));
+};
 
 module.exports = {
   on: on,
