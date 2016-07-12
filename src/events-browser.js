43a1a53c4e5489c4ce8016e8ee0b7609e85ee159
André Filipe Goulart <andre.goulart@b2wdigital.com>
2016-07-12 13:15:26 -0300

    change const by var

--- src/events-browser.js
+++ src/events-browser.js
@@ -2,13 +2,13 @@
 
 require('./events-browser-polyfill');
 
-const on = function(evt, params, options) {
-  const target = options && options.target ? options.target : window.document;
+var on = function(evt, params, options) {
+  var target = options && options.target ? options.target : window.document;
   return target.addEventListener(evt, params);
 };
 
-const emit = function(evt, params, options) {
-  const target = options && options.target ? options.target : window.document;
+var emit = function(evt, params, options) {
+  var target = options && options.target ? options.target : window.document;
   return target.dispatchEvent(new window.CustomEvent(evt, params));
 };
 
